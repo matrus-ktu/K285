a64197ba17937942bf46c4fe8aa46025b0380eea
Paulius Skliaudys <paulius.skliaudys@ktu.edu>
2023-03-12 10:59:25 +0200

    Fixed 'Juridinis asmuo' functionality

--- static/js/main.js
+++ static/js/main.js
@@ -52,43 +52,6 @@
             $dropdown.off("mouseenter mouseleave");
         }
     });
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        dots: false,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
     
 })(jQuery);
 
